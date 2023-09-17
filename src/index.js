c7758f8811d34c7faf0b25aab3dbadc281ed72b8
Alexandria Webb <alexandriakwebb@gmail.com>
2023-09-17 10:11:49 -0600

    builds functional to-do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task.value, e.target.task_priority.value)
+    form.reset()
+  })
+
+  function buildToDo(todo, selection) {
+    let myTask = document.createElement('li')
+    let completeTask = document.createElement('button')
+    completeTask.addEventListener('click', handleDelete)
+    completeTask.textContent = 'Done!'
+    myTask.textContent = `${todo} is ${selection} priority `
+    myTask.appendChild(completeTask)
+    console.log(myTask)
+    if (selection === 'high'){
+      myTask.style.color = 'red'
+    } else if (selection === 'medium'){
+      myTask.style.color = 'orange'
+    } else {myTask.style.color = 'green'}
+    document.querySelector('#tasks').appendChild(myTask)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
+
+})
